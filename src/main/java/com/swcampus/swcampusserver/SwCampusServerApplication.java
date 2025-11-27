779396b8a3d607a9b690dd6678bd0b87c068ad81
kkkwp <chxrryda@gmail.com>
2025-11-27 12:15:31 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/swcampus/swcampusserver/SwCampusServerApplication.java
@@ -0,0 +1,13 @@
+package com.swcampus.swcampusserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwCampusServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwCampusServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swcampus/swcampusserver/SwCampusServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swcampus.swcampusserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwCampusServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
