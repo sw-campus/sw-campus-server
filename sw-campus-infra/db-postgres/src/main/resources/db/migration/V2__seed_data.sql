a2534f9c1b6c4a08267f46abac6dc80e0fecea7e
zionge2k <zion.geek.py@gmail.com>
2025-12-30 17:13:29 +0900

    logback, V2 sql 수정

--- sw-campus-infra/db-postgres/src/main/resources/db/migration/V2__seed_data.sql
+++ sw-campus-infra/db-postgres/src/main/resources/db/migration/V2__seed_data.sql
@@ -6,7 +6,7 @@
 -- ========================================
 -- 1. Admin User
 -- ========================================
-INSERT INTO swcampus.members (user_id, email, name, role, created_at, updated_at, password) VALUES (1, 'admin@swcampus.com', 'Admin', 'ADMIN', NOW(), NOW(), '$2b$10$iBA5qPX7qtKqaAbTNOkP5OHgCeLays0lsOHG.W07objGW27hZQ7K.') ON CONFLICT DO NOTHING;
+INSERT INTO swcampus.members (user_id, email, name, role, created_at, updated_at, password) VALUES (1, 'admin@softwarecampus.co.kr', 'Admin', 'ADMIN', NOW(), NOW(), '$2b$10$iBA5qPX7qtKqaAbTNOkP5OHgCeLays0lsOHG.W07objGW27hZQ7K.') ON CONFLICT DO NOTHING;
 SELECT setval('swcampus.members_user_id_seq', (SELECT COALESCE(MAX(user_id), 1) FROM swcampus.members));
 
 -- ========================================
